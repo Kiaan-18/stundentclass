44c82c139bb09d42ffc266ef100ceafb77f34e1b
Kiaan-18 <kapse.kiaan@gmail.com>
2025-07-13 10:43:42 +0200

    Add files via upload

---
+++ vehicleclass.py
@@ -0,0 +1,11 @@
+class Vehicle:
+    def __init__(self,max_speed,mileage):
+        self.max_speed1=max_speed
+        self.mileage1=mileage
+modelX= Vehicle(240,18)
+model2= Vehicle(300,40)
+print("Model Max Speed:",modelX.max_speed1)
+print("Model Mileage :",modelX.mileage1)
+print("Model Max speed:",model2.max_speed1)
+print("Model Mileage:",model2.mileage1)
+            
